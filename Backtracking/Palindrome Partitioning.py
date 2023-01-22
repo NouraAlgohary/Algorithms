6432264cced00ba079b969d56607c7e1fe7f6693
Noura <noura.malgohary@gmail.com>
2023-01-22 13:14:04 +0200

    Add Palindrome Partitioning

---
+++ Backtracking/Palindrome Partitioning.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s, st, end) -> bool:
+        while st < end:
+            if s[st] != s[end]:
+                return False
+            st += 1
+            end -= 1
+        return True
+
+    def allPossiblePalindrome(self, palindrome, s, idx, branch):
+        if len(s) == idx:
+            palindrome.append(branch[:])
+            return
+        
+        for i in range(idx, len(s)):
+            if self.isValid(s, idx, i):
+                branch.append(s[idx:i+1])
+                self.allPossiblePalindrome(palindrome, s, i+1, branch)
+                branch.pop(-1)
+
+    def partition(self, s: str) -> List[List[str]]:
+        palindrome = []
+        branch = []
+        self.allPossiblePalindrome(palindrome, s, 0, branch)
+        return palindrome
